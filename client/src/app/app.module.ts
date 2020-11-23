aa1945256127455c8a9e57224db3b6f72a103f99
jorgechp <jorgechp@yandex.com>
2020-11-24 00:34:32 +0100

    Sign up implemented

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
@@ -50,6 +51,7 @@ import { WelcomeComponent } from '@src/app/components/welcome/welcome.component'
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     // AppMaterialModule,
     MatButtonModule,
     MatIconModule,
--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, AbstractControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { UserService } from '@src/app/services/user.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -8,14 +10,17 @@ import { Validators } from '@angular/forms';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
 
   loginForm: FormGroup;
   registerForm: FormGroup;
 
   hide = true;
 
-  constructor(private formBuilder: FormBuilder) {
+  createUserSuscription: Subscription;
+
+  constructor(private formBuilder: FormBuilder,
+              private _userService: UserService) {
     this.loginForm = formBuilder.group({
       login_nick: new FormControl('', [Validators.required]),
       login_password: new FormControl('', [Validators.required])
@@ -30,15 +35,35 @@ export class SignupComponent implements OnInit {
    }
 
   ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.createUserSuscription.unsubscribe();
+  }
 
   get login_form() { return this.loginForm.controls; }
   get signup_form() { return this.registerForm.controls; }
 
   loginSubmit(): void{
-    
+    if(this.loginForm.valid){
+      console.log('test');
+    }
   }
 
   registerSubmit(): void{
+    if(this.registerForm.valid){
+
+      const username = this.registerForm.get('signup_nick');
+      const nick = this.registerForm.get('signup_nick');
+      const mail = this.registerForm.get('signup_mail');
+      const password = this.registerForm.get('signup_password');
+
+      if (username && nick && mail && password){
+        this.createUserSuscription = this._userService.registerUser(nick.value, mail.value, password.value).subscribe(
+          (response: object) => {
+            console.log(response);
+          }
+        );
+      }
+    }
 
   }
 
