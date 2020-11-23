d5b20be41ec459de68e61aea6e326e51ae94ebb2
jorchamo <jorge.chamorro@t-systems.com>
2020-11-23 17:13:10 +0100

    Componets and signup form

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TrainComponent } from '@src/app/components/train/train.component';
+import { StatsComponent } from '@src/app/components/stats/stats.component';
+import { HelpComponent } from '@src/app/components/help/help.component';
+import { AboutComponent } from '@src/app/components/about/about.component';
+import { WelcomeComponent } from '@src/app/components/welcome/welcome.component';
+import { SignupComponent } from '@src/app/components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'train', component: TrainComponent},
+  {path: 'stats', component: StatsComponent},
+  {path: 'help', component: HelpComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/components/signup/signup.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, AbstractControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+
+  hide = true;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      login_nick: new FormControl('', [Validators.required]),
+      login_password: new FormControl('', [Validators.required])
+    });
+
+    this.registerForm = formBuilder.group({
+      signup_nick: new FormControl('', [Validators.required]),
+      signup_mail: new FormControl('', [Validators.required, Validators.email]),
+      signup_password: new FormControl('', [Validators.required]),
+      signup_password2: new FormControl('', [Validators.required])
+    });
+   }
+
+  ngOnInit(): void {}
+
+  get login_form() { return this.loginForm.controls; }
+  get signup_form() { return this.registerForm.controls; }
+
+}
