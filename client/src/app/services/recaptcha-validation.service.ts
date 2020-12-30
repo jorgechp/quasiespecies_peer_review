681af415291e4e6bfef319b1fbe174edc731f930
jorgechp <jorgechp@yandex.com>
2020-12-31 00:27:00 +0100

    Set token validation port

--- client/src/app/services/recaptcha-validation.service.ts
+++ client/src/app/services/recaptcha-validation.service.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { Injectable, OnDestroy } from '@angular/core';
 import {HttpClient } from '@angular/common/http'
+import { CONFIG } from '@src/app/config';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class RecaptchaValidationService {
 
 
   sendToken(token: string): Observable<any>{
-    return this.http.post<any>('/token_validate', {recaptcha: token});
+    return this.http.post<any>(CONFIG.TOKEN_VALIDATOR_HOST + '/token_validate' + ':' + CONFIG.TOKEN_VALIDATOR_PORT, {recaptcha: token});
   }
 }
